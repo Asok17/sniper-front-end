9bdc2ca25458042416f3e944fdbbaa4b4deab370
Anuj Subedi <stoneera111@gmail.com>
2017-11-16 18:12:02 +0545

    Upload framework
    Upload framework - initial upload of framework files

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php 
+
+/*
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Package @ Sniper front-end
+
+# Author @ Anuj Subedi
+
+# This is index page to include the templet parts
+
+# Since 2017 Nov 
+
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+**/
+
+
+// Include header of project
+
+include 'header.html';
+
+// Include body of project file
+
+include 'body.html';
+
+// Include sidebar of the project file
+
+include 'sidebar.html';
+
+// Include footer of project
+
+include 'footer.html';
---
+++ readme.txt
@@ -0,0 +1,14 @@
+/*
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Package @ Sniper front-end
+
+# Author @ Anuj Subedi
+
+# This is index page to include the templet parts
+
+# Since 2017 Nov 
+
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+**/
